81803d27c627b938b4d840a4d8b0e4a8330cd9e3
wumingyu <317808023@qq.com>
2016-09-27 11:24:28 +0800

    删除除user外的model
    Signed-off-by: wumingyu <317808023@qq.com>

--- src/server/service/user.go
+++ src/server/service/user.go
@@ -47,10 +47,10 @@ func (self *UserService) Create(user *model.User) (*model.User, error) {
 		common.Logger.Warningln("注册新用户,插入记录失败:", r.Error.Error())
 		return nil, r.Error
 	}
-	return user, nil;
+	return user, nil
 }
 
 func (self *UserService) Update(user *model.User) (bool, error) {
 	common.DB.Model(&user).Update("nick_name", "change")
-	return true, nil;
+	return true, nil
 }
