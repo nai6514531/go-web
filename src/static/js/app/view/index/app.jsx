7be14a57ce04789695ed1f34d2085ecd39855e75
Arco1ris <longhmail@gmail.com>
2016-10-08 12:29:42 +0800

    添加菜单栏布局和样式

--- src/static/js/app/view/index/app.jsx
+++ src/static/js/app/view/index/app.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './app.less';
 
 import { Head } from './head/app';
+import { LeftMenu } from './menu/app';
+
 
 const App = React.createClass({
   render() {
     return (
       <div className="index">
 		  <Head/>
+		  <LeftMenu/>
       </div>
     );
   }
