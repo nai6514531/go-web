472739fdd21faa64acdb204a74d1c054637da8ca
Table <biaoqier@qq.com>
2016-10-22 20:33:10 +0800

    1：对旧数据库用户ID做加1处理； 2：调整用户角色同步逻辑；

--- src/server/service/user_role_rel.go
+++ src/server/service/user_role_rel.go
@@ -18,6 +18,15 @@ func (self *UserRoleRelService) Basic(id int) (*model.UserRoleRel, error) {
 	return userRoleRel, nil
 }
 
+func (self *UserRoleRelService) BasicByUserId(userId int) (*model.UserRoleRel, error) {
+	userRoleRel := &model.UserRoleRel{}
+	r := common.DB.Where("user_id = ? ", userId).First(userRoleRel)
+	if r.Error != nil {
+		return nil, r.Error
+	}
+	return userRoleRel, nil
+}
+
 func (self *UserRoleRelService) BasicByUserIdAndRoleId(userId int, roleId int) (*model.UserRoleRel, error) {
 	userRoleRel := &model.UserRoleRel{}
 	r := common.DB.Where("user_id = ? and role_id=?", userId, roleId).First(userRoleRel)
