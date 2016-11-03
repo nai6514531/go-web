e63e525c3b126de6e4f645df10aa16b6600c3379
Arco1ris <longhmail@gmail.com>
2016-11-03 11:38:01 +0800

    兼容 IE9

--- src/static/js/app/main.jsx
+++ src/static/js/app/main.jsx
@@ -1,7 +1,10 @@
+require('es6-promise').polyfill();
+import 'babel-polyfill';
+
 import 'whatwg-fetch';
 import ReactDOM from 'react-dom';
 import router from './router.jsx';
-import 'nprogress/nprogress.css'
+import 'nprogress/nprogress.css';
 ReactDOM.render(router, document.getElementById('main'));
 import 'antd/dist/antd.min.css';
 
