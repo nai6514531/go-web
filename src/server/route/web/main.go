c69a6b047eb29dc9f4c3670ae3ef4bd0a0f4d6fe
Table <biaoqier@qq.com>
2016-11-05 16:43:25 +0800

    更新基础镜像

--- src/server/route/web/main.go
+++ src/server/route/web/main.go
@@ -3,6 +3,7 @@ package web
 import (
 	"github.com/kataras/iris"
 	"maizuo.com/soda-manager/src/server/controller/web"
+	"maizuo.com/soda-manager/src/server/enity"
 )
 
 func Web() {
@@ -19,6 +20,29 @@ func Web() {
 	iris.Get("/500", func(ctx *iris.Context) {
 		panic(500)
 		ctx.EmitError(iris.StatusInternalServerError)
+	})
+
+	iris.Get("/ip-test", func(ctx *iris.Context) {
+
+		type IP struct {
+			LocalAddr     string
+			RemoteAddr    string
+			RequestIP     string
+			XForwardedFor string
+			XRealIP       string
+		}
+
+		ip := &IP{
+			ctx.LocalAddr().String(),
+			ctx.RemoteAddr(),
+			ctx.RequestIP(),
+			ctx.RequestHeader("X-Forwarded-For"),
+			ctx.RequestHeader("X-Real-IP"),
+		}
+
+		result := &enity.Result{"0", ip, "", }
+
+		ctx.JSON(iris.StatusOK, result)
 	})
 
 }
