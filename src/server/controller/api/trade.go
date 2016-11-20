a7853874abc9b19a7d80ea339eab270f69282fd7
ferry <ferry@hyx.com>
2016-11-20 22:37:04 +0800

    退款接口

--- src/server/controller/api/trade.go
+++ src/server/controller/api/trade.go
@@ -13,6 +13,9 @@ var (
 		"01080101": "拉取模块数据失败",
 		"01080102": "缺少参数",
 
+		"01080200": "退款成功",
+		"01080201": "退款失败",
+		"01080202": "参数错误",
 	}
 )
 
@@ -45,3 +48,31 @@ func (self *TradeController) Basic(ctx *iris.Context) {
 	ctx.JSON(iris.StatusOK, result)
 }
 
+func (self *TradeController) refund(ctx *iris.Context) {
+	result := &enity.Result{}
+	tradeService := *service.TradeService{}
+	washId, err := ctx.URLParamInt("washId")
+	if err != nil {
+		result = &enity.Result{"01080202", nil, trade_msg["01080202"]}
+		common.Log(ctx, result)
+		ctx.JSON(iris.StatusOK, result)
+		return
+	}
+	account := ctx.URLParam("account")
+	if washId < 0 || account == "" {
+		result = &enity.Result{"01080202", nil, trade_msg["01080202"]}
+		common.Log(ctx, result)
+		ctx.JSON(iris.StatusOK, result)
+		return
+	}
+	_, err = tradeService.Refund(washId, account)
+	if err != nil {
+		result = &enity.Result{"01080201", err.Error(), trade_msg["01080201"]}
+		common.Log(ctx, result)
+		ctx.JSON(iris.StatusOK, result)
+		return
+	}
+	result = &enity.Result{"01080200", nil, statis_msg["01080200"]}
+	common.Log(ctx, nil)
+	ctx.JSON(iris.StatusOK, result)
+}
