4abb8a7e1cbe2f52088386d7ad3317b1b6c9037f
ferry <ferry@hyx.com>
2016-11-28 11:50:18 +0800

    修改账单账户信息没有值的字段无返回问题

--- src/server/model/daily_bill.go
+++ src/server/model/daily_bill.go
@@ -12,12 +12,12 @@ type DailyBill struct {
 	Status      int    `json:"status"` //0:未申请提现or未结账 1:已申请提现 2:已结账 3:结账中 4:结账失败
 	SubmitAt    time.Time `json:"submitAt"`
 	OrderCount  int    `json:"orderCount"`
-	AccountType int    `json:"accountType,omitempty" gorm:"-"`
-	AccountName string `json:"accountName,omitempty" gorm:"-"`
-	Account     string `json:"account,omitempty" gorm:"-"`
-	RealName    string `json:"realName,omitempty" gorm:"-"`
-	BankName    string `json:"bankName,omitempty" gorm:"-"`
-	Mobile      string `json:"mobile,omitempty" gorm:"-"`
+	AccountType int    `json:"accountType" gorm:"-"`
+	AccountName string `json:"accountName" gorm:"-"`
+	Account     string `json:"account" gorm:"-"`
+	RealName    string `json:"realName" gorm:"-"`
+	BankName    string `json:"bankName" gorm:"-"`
+	Mobile      string `json:"mobile" gorm:"-"`
 }
 
 func (DailyBill) TableName() string {
