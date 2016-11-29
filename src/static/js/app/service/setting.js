c3819c672894c392c3f5f7e9b1a946b7f6f31193
Arco1ris <longhmail@gmail.com>
2016-11-29 22:33:55 +0800

    修改路由,修复BUG

--- src/static/js/app/service/setting.js
+++ src/static/js/app/service/setting.js
@@ -2,7 +2,7 @@ import api from '../library/request/api'
 
 const Service = {
   changePassword: (data) => {
-    return api.put(`/api/user/password?current=${data.current}&newer=${data.newer}`, {})
+    return api.put(`/api/user-password?current=${data.current}&newer=${data.newer}`, {})
   },
 };
 
