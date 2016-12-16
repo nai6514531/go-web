731d66dd1a991cea4a3e061ca022e4dad1ff0fbe
ferry <ferry@hyx.com>
2016-12-16 18:33:47 +0800

    修改导出提示框样式

--- src/static/js/app/view/settlement/list/export-bill.jsx
+++ src/static/js/app/view/settlement/list/export-bill.jsx
@@ -15,6 +15,7 @@ const FormList = React.createClass({
 		const exportUrl = this.props.exportUrl;
 		return (
 			<form name="exportbill" action={exportUrl} method="post" target="_blank">
+        <span className="form-text">确认导出这批账单吗？</span>
         <button onClick={this.cancel} type="button" id="cancel">取消</button>
         <button onClick={this.submit} type="submit" id="submit">确认</button>
       </form>
