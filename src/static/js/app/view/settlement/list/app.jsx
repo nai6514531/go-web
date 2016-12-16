347158fd054023d108027053e31d1449d77a8f16
ferry <ferry@hyx.com>
2016-12-16 18:33:14 +0800

    1.修改导出路径;2.修改结算管理文案和样式

--- src/static/js/app/view/settlement/list/app.jsx
+++ src/static/js/app/view/settlement/list/app.jsx
@@ -866,9 +866,10 @@ const App = React.createClass({
             locale={zhCN}
           />
           <Input defaultValue={defaultValue.userOrBank} style={{width: 160}} className="item" placeholder="输入运营商名称或者银行名称或户名" onChange={this.handleUserOrBankChange}/>
-          <Button className="item" type="primary" icon="search" onClick={this.handleFilter}>筛选</Button>
-          <Button className="item" type="primary" icon="download" onClick={this.exportBill}>导出</Button>
           {USER.role.id == 3 ?
+            <Button className="item" type="primary" icon="download" onClick={this.exportBill}>导出</Button>
+          :""}
+            {USER.role.id == 3 ?
             <Button className="calculate item" onClick={this.CalculateAmount} type="primary">计算金额</Button>
             :""}
         </div>
