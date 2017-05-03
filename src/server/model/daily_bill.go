d02ad19b29531c9fd6eb639dcf2c45a0efbaf07d
Martin <martin@maizuo.com>
2017-05-03 12:04:52 +0800

    为用户交易账户增加银行总行字段

--- src/server/model/daily_bill.go
+++ src/server/model/daily_bill.go
@@ -4,21 +4,22 @@ import "time"
 
 type DailyBill struct {
 	Model
-	UserId      int       `json:"userId"`
-	UserName    string    `json:"userName"`
-	TotalAmount int       `json:"totalAmount"`
-	SettledAt   string    `json:"settledAt"`
-	BillAt      string    `json:"billAt"`
-	Status      int       `json:"status"` //0:未申请提现or未结账 1:已申请提现 2:已结账 3:结账中 4:结账失败
-	SubmitAt    time.Time `json:"submitAt"`
-	OrderCount  int       `json:"orderCount"`
-	AccountType int       `json:"accountType"`
-	AccountName string    `json:"accountName"`
-	Account     string    `json:"account"`
-	RealName    string    `json:"realName"`
-	BankName    string    `json:"bankName"`
-	Mobile      string    `json:"mobile"`
-	HasMarked   int       `json:"hasMarked"`
+	UserId       int       `json:"userId"`
+	UserName     string    `json:"userName"`
+	TotalAmount  int       `json:"totalAmount"`
+	SettledAt    string    `json:"settledAt"`
+	BillAt       string    `json:"billAt"`
+	Status       int       `json:"status"` //0:未申请提现or未结账 1:已申请提现 2:已结账 3:结账中 4:结账失败
+	SubmitAt     time.Time `json:"submitAt"`
+	OrderCount   int       `json:"orderCount"`
+	AccountType  int       `json:"accountType"`
+	AccountName  string    `json:"accountName"`
+	Account      string    `json:"account"`
+	RealName     string    `json:"realName"`
+	BankHeadName string    `json:"bankHeadName"`
+	BankName     string    `json:"bankName"`
+	Mobile       string    `json:"mobile"`
+	HasMarked    int       `json:"hasMarked"`
 }
 
 func (DailyBill) TableName() string {
