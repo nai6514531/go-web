540b947e21d24287feb74ab9e002370304801526
zhoutianlei <18234101603@163.com>
2017-05-10 10:23:37 +0800

    增加没勾选时候的条件判断

--- src/static/js/app/view/settlement/list/app.jsx
+++ src/static/js/app/view/settlement/list/app.jsx
@@ -649,12 +649,16 @@ const App = React.createClass({
 		let data = this.getCheckedData();
 		let selectedList = this.state.selectedList;
 		let len = selectedList.length;
-		for (let i = 0; i < len; i++) {
-			let item = selectedList[i];
-			if ((item.accountType != 1 || item.status != 4) && (item.account || item.realName || item.mobile)) {
-				message.info("您所勾选的不是异常账单");
-				return;
+		if (len) {
+			for (let i = 0; i < len; i++) {
+				let item = selectedList[i];
+				if ((item.accountType != 1 || item.status != 4) && item.account) {
+					message.info("您所勾选的不是异常账单");
+					return;
+				}
 			}
+		} else {
+			return;
 		}
 		DailyBillService.updateAbnormalBill(data).then((res) => {
 			this.setState({
