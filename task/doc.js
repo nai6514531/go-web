7bf42455d52593b2f0942eec2797b076bd0eb063
Martin <martin@maizuo.com>
2017-06-05 12:29:02 +0800

    api文档改动

--- task/doc.js
+++ task/doc.js
@@ -2,5 +2,5 @@ const gulp = require('gulp');
 const shell = require('gulp-shell');
 
 gulp.task('doc', shell.task([
-  'aglio -i ./api.md --theme-variables flatly --theme-template triple -s',
+  'aglio -i ./api.md --theme-variables slate -s',
 ]));
