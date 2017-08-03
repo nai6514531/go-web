3deab0830369fccbb58be1a050b1f6eeaa281e74
l_sivan <907616591@qq.com>
2017-08-03 10:09:44 +0800

    1.daily_bill增加string类型的bill_id字段2.将bill_rel和bill_batch_no的bill_id换成string

--- src/server/model/bill_rel.go
+++ src/server/model/bill_rel.go
@@ -2,12 +2,12 @@ package model
 
 type BillRel struct {
 	Model
-	BillId      int         `json:"billId"`
-	BatchNo     string      `json:"batchNo"`
-	Type        int         `json:"type"`
-	IsSuccessed bool        `json:"isSuccessed"`
-	Reason      string      `json:"reason"`
-	OuterNo     string      `json:"outerNo"`
+	BillId      string `json:"billId"`
+	BatchNo     string `json:"batchNo"`
+	Type        int    `json:"type"`
+	IsSuccessed bool   `json:"isSuccessed"`
+	Reason      string `json:"reason"`
+	OuterNo     string `json:"outerNo"`
 }
 
 func (BillRel) TableName() string {
