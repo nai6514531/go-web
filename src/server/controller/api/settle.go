ef8422be8135a2f3a01ffb0464db694cb6c09b45
l_sivan <907616591@qq.com>
2017-08-15 16:57:03 +0800

    1.补日志的坑2.调整支付方式以及手续费的逻辑结构

--- src/server/controller/api/settle.go
+++ src/server/controller/api/settle.go
@@ -1,52 +1,54 @@
 package controller
 
 import (
-	"gopkg.in/kataras/iris.v4"
 	"github.com/spf13/viper"
+	"gopkg.in/kataras/iris.v4"
+	"maizuo.com/soda-manager/src/server/common"
 	"maizuo.com/soda-manager/src/server/enity"
 	"maizuo.com/soda-manager/src/server/service"
-	"maizuo.com/soda-manager/src/server/common"
 )
 
 type SettleController struct {
+}
 
-}
-/*{
-    "status": 01110000,
-    "data": {
-        "totalAmount": 1000
-        "count": 2  // 结算天数
-    },
-    "msg": "拉取可结算信息成功"
-}
-*/
 // 获取可提现金额及其明细信息
-func (self *SettleController) SettlementDetails(ctx *iris.Context){
+func (self *SettleController) SettlementDetails(ctx *iris.Context) {
 	dailyBillService := &service.DailyBillService{}
 	userCashAccountService := &service.UserCashAccountService{}
 	userId := ctx.Session().Get(viper.GetString("server.session.user.id")).(int)
-	amount,count,err := dailyBillService.CountAllocatableMoneyByUserId(userId)
-	if err != nil  {
-		common.Logger.Debugln("CountAllocatableMoney CountAllocatableMoneyByUserId error",err)
-		ctx.JSON(iris.StatusOK,&enity.Result{"01110001", err, daily_bill_msg["01110001"]})
-		return
-	}
-	accountMap,err := userCashAccountService.BasicMapByUserId(userId)
+	amount, count, err := dailyBillService.CountAllocatableMoneyByUserId(userId)
 	if err != nil {
-		common.Logger.Debugln("CountAllocatableMoney BasicMapByUserId error",err)
-		ctx.JSON(iris.StatusOK,&enity.Result{"01110001", err, daily_bill_msg["01110001"]})
+		result := &enity.Result{"01110001", err, daily_bill_msg["01110001"]}
+		ctx.JSON(iris.StatusOK, result)
+		common.Log(ctx, result)
 		return
 	}
-	cast := viper.GetInt("bill.cast")
-	if !(accountMap[userId].Type == 1 && amount <= viper.GetInt("bill.borderValue")) {
-		// 如果不是支付宝且金额少于200的情况
-		cast = amount*viper.GetInt("bill.rate")/100
+	userCashAccount, err := userCashAccountService.BasicByUserId(userId)
+	if err != nil {
+		result := &enity.Result{"01110001", err, daily_bill_msg["01110001"]}
+		ctx.JSON(iris.StatusOK, result)
+		common.Log(ctx, result)
+		return
 	}
-
-	ctx.JSON(iris.StatusOK,&enity.Result{"01110000", map[string]interface{}{
+	// 默认值用支付宝低于200的情况
+	cast, rate := viper.GetInt("bill.aliPay.cast"), viper.GetInt("bill.aliPay.rate")
+	if userCashAccount.Type == 1 {
+		// 支付宝大于200的情况
+		if amount > viper.GetInt("bill.aliPay.borderValue") {
+			rate = 1
+			cast = amount * rate / 100
+		}
+	} else {
+		// 微信
+		rate = viper.GetInt("bill.wechat.rate")
+		cast = amount * rate / 100
+	}
+	result := &enity.Result{Status: "01110000", Data: map[string]interface{}{
 		"totalAmount": amount,
-		"count": count,
-		"cast":cast,
-
-	}, daily_bill_msg["01110000"]})
+		"count":       count,
+		"cast":        cast,
+	}, Msg: daily_bill_msg["01110000"]}
+	ctx.JSON(iris.StatusOK, result)
+	common.Log(ctx, result)
+	return
 }
